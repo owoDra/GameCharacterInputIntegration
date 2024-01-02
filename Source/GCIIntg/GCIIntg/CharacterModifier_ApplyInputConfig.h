c9522bebaa1c531bf5a22afd7c36e92c810864d5
owoDra <85442566+owoDra@users.noreply.github.com>
2024-01-02 22:17:53 +0900

    バグ修正

---
+++ Source/GCIIntg/GCIIntg/CharacterModifier_ApplyInputConfig.h
@@ -0,0 +1,30 @@
+﻿// Copyright (C) 2023 owoDra
+
+#pragma once
+
+#include "CharacterModifier.h"
+
+#include "CharacterModifier_ApplyInputConfig.generated.h"
+
+class UPlayableComponent;
+class UInputConfig;
+
+
+/**
+ * Modifier class to apply InputConfig to Playable component
+ */
+UCLASS(meta = (DisplayName = "CM Apply Input Config"))
+class UCharacterModifier_ApplyInputConfig final : public UCharacterModifier
+{
+	GENERATED_BODY()
+public:
+	UCharacterModifier_ApplyInputConfig();
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "ApplyInputConfig")
+	TSoftObjectPtr<UInputConfig> InputConfig{ nullptr };
+
+protected:
+	virtual bool OnApply(APawn* Pawn) const override;
+
+};
